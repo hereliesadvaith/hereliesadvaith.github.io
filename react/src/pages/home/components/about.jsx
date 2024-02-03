e336056380b73947539b34d26b36b2001d9d60b5
Advaith B G <hereliesadvaith@gmail.com>
2024-02-03 20:45:08 +0530

    tools home

--- react/src/pages/home/components/about.jsx
+++ react/src/pages/home/components/about.jsx
@@ -1,13 +1,10 @@
 import Constants from "../shared/constants"
-import { BiSolidQuoteAltLeft, BiSolidQuoteAltRight } from "react-icons/bi"
 
 export default function About() {
   return (
     <div className="mt-5 flex flex-col items-center">
-      <h2 className="text-[36px] font-bold max-w-[700px] text-center">{Constants.ABOUT_HEADING}</h2>
-      <BiSolidQuoteAltLeft className="bg-red-600 p-3 text-[44px] rounded-full mt-6 text-white"/>
-      <h2 className="my-5 text-gray-400 text-[20px] max-w-[700px] text-center">{Constants.ABOUT_DESC}</h2>
-      <BiSolidQuoteAltRight className="bg-red-600 p-3 text-[44px] rounded-full mt-6 text-white"/>
+      {Constants.ABOUT_DESC}
+      {Constants.ABOUT_TOOLS}
     </div>
   )
 }
--- react/src/pages/home/shared/constants.jsx
+++ react/src/pages/home/shared/constants.jsx
@@ -1,4 +1,34 @@
 export default {
   ABOUT_HEADING: "Python Developer based in Kerala.",
-  ABOUT_DESC: "I love coding because it allows me to solve problems and be creative at the same time.aaaaaaaa aaaaaa aaaaaaaa aaaaaaaaaaa aaaaaaaa aaaaaaaa aaaaaaaaa aaaaaaaa aaaaa aaaaa aaaaa aaaaaaaaaaaa aaaaaaaaa aaaaaaaaaa aaa aaaaaaa aaaaa aaaaaaa aaaaa aaaaaa aaaaaaaa aaaaaaa aaaaaaaa aaaaaaa aaaaa aaaaaaaa aaaaaaaaa aaaaaaa aaaaa aaaaaaaa aaa aaaa aaaaaa aaaaaaaa aaaaaa aaaa a a aaa aaa aaaaaaaaaaa aaaaaaaa aaaaaaaaaaa aaaaaaaaaaaa aaaaaaaaa aaaaaaaaaaa aaaaaaaaaa aaaaaaaaaa aaaaaaa aaaaaaaaa aaaaaaa aaaaaaaa aaaaaaaaaa aaaaaaa aaaaaaaaa aaaaaaa aa aaaaaa aaaaaaaaa aaaaa aaaaaaa aaaaaaaaaaa"
+  ABOUT_DESC: <h3 className="my-5 text-gray-400 text-[22px] max-w-[700px]">
+    Hi Everyone, I am <span className="bg-gradient-to-b from-red-500 to-gray-400 bg-clip-text text-transparent">Advaith </span>
+    from Kerala, India. I am currently employed as a <span className="bg-gradient-to-b from-red-500 to-gray-400 bg-clip-text
+    text-transparent">Python developer</span> at Cybrosys Technologies.<br/><br/>
+    In my free time, I enjoy gaming a little bit and working on personal projects in <span className="bg-gradient-to-b from-red-500 to-gray-400 bg-clip-text text-transparent">Algorithmic trading. </span>
+    I'm excited to start my career in Data Science and continue learning and growing in this field.
+  </h3>,
+  ABOUT_TOOLS: <div className="tools my-10">
+                  <div className="row flex justify-center">
+                    <img src="https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54"/>
+                    <img src="https://img.shields.io/badge/go-%2300ADD8.svg?style=for-the-badge&logo=go&logoColor=white"/>
+                  </div>
+                  <div className="row flex justify-center">
+                    <img src="https://img.shields.io/badge/scrapy-%236DB33F.svg?style=for-the-badge&logo=scrapy&logoColor=white"/>  
+                    <img src="https://img.shields.io/badge/postgres-%23316192.svg?style=for-the-badge&logo=postgresql&logoColor=white"/>
+                    <img src="https://img.shields.io/badge/mysql-%2300f.svg?style=for-the-badge&logo=mysql&logoColor=white"/>
+                  </div>
+                  <div className="row flex justify-center">
+                    <img src="https://img.shields.io/badge/numpy-%23013243.svg?style=for-the-badge&logo=numpy&logoColor=white"/>
+                    <img src="https://img.shields.io/badge/pandas-%23150458.svg?style=for-the-badge&logo=pandas&logoColor=white"/>
+                    <img src="https://img.shields.io/badge/scikit--learn-%23F7931E.svg?style=for-the-badge&logo=scikit-learn&logoColor=white"/>
+                  </div> 
+                  <div className="row flex justify-center">
+                    <img src="https://img.shields.io/badge/NeoVim-%2357A143.svg?&style=for-the-badge&logo=neovim&logoColor=white"/>
+                    <img src="https://img.shields.io/badge/VS%20Code-0078d7.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white)"/>
+                  </div>
+                  <div className="row flex justify-center">
+                    <img src="https://img.shields.io/badge/Debian-D70A53?style=for-the-badge&logo=debian&logoColor=white"/>
+                  </div>
+                </div>
+                  
 }
