ceff8ba55ca97ed8f0a9a263751570f22c7bb441
Advaith B G <hereliesadvaith@gmail.com>
2024-12-07 20:52:23 +0530

    [ADD] Nav items

--- portfolio/components/Header.jsx
+++ portfolio/components/Header.jsx
@@ -5,7 +5,7 @@ import Nav from "./Nav";
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
-      <div className="container mx-auto flex justify-between">
+      <div className="container mx-auto flex justify-between items-center">
         {/* Logo */}
         <Link href="/">
           <h1 className="text-4xl font-semibold">
@@ -21,7 +21,7 @@ const Header = () => {
         </div>
         {/* Mobile Nav */}
         <div className="xl:hidden">
-
+          mobile-nav
         </div>
       </div>
     </header>
--- portfolio/components/Nav.jsx
+++ portfolio/components/Nav.jsx
@@ -1,5 +1,51 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "Services",
+    href: "/services",
+  },
+  {
+    name: "Resume",
+    href: "/resume",
+  },
+  {
+    name: "Work",
+    href: "/work",
+  },
+  {
+    name: "Blog",
+    href: "/blog",
+  },
+];
+
 const Nav = () => {
-  return <nav>desktop nav</nav>;
+  const pathname = usePathname();
+  return (
+    <nav className="flex gap-8">
+      {links.map((link, index) => {
+        return (
+          <Link
+            href={link.href}
+            key={index}
+            className={`${
+              link.href === pathname &&
+              "text-accent-default border-b-2 border-accent-default"
+            } capitalize font-medium hover:text-accent-default transition-all`}
+          >
+            {link.name}
+          </Link>
+        );
+      })}
+    </nav>
+  );
 };
 
 export default Nav;
