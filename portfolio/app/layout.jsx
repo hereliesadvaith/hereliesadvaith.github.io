f6d7a59f692def690bd5d52a290cc1ff4f51c230
Advaith B G <hereliesadvaith@gmail.com>
2024-12-10 20:50:41 +0530

    [ADD] framer-motion: staircase transition

--- portfolio/app/layout.jsx
+++ portfolio/app/layout.jsx
@@ -1,8 +1,9 @@
 import "./globals.css";
 
 // Components
-import Header from "../components/Header";
+import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
 export const metadata = {
   title: "hereLiesAdvaith",
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <Header />
+        <StairTransition />
         <PageTransition>{children}</PageTransition>
       </body>
     </html>
--- portfolio/components/StairTransition.jsx
+++ portfolio/components/StairTransition.jsx
@@ -2,27 +2,32 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 
-const PageTransition = ({ children }) => {
+// Components
+import Stairs from "@/components/Stairs";
+
+const StairTransition = () => {
   const pathname = usePathname();
   return (
-    <AnimatePresence>
+    <AnimatePresence mode="wait">
       <div key={pathname}>
+        <div className="h-screen w-screen fixed top-0 z-40 flex pointer-events-none">
+          <Stairs />
+        </div>
         <motion.div
+          className="h-screen w-screen fixed top-0 bg-primary pointer-events-none"
           initial={{ opacity: 1 }}
           animate={{
             opacity: 0,
             transition: {
-              delay: 0.5,
-              duration: 0.5,
+              delay: 1,
+              duration: 0.4,
               ease: "easeInOut",
             },
           }}
-          className="h-screen w-screen fixed top-0 bg-primary pointer-events-none"
         />
-        {children}
       </div>
     </AnimatePresence>
   );
 };
 
-export default PageTransition;
+export default StairTransition;
