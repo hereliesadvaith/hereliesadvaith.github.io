6d45032dac5f877197034079195cb0f3b030790a
Advaith B G <hereliesadvaith@gmail.com>
2025-01-06 22:57:23 +0530

    [ADD] Header

---
+++ portfolio/components/Header.jsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+// Components
+import { Button } from "@/components/ui/button";
+import Nav from "@/components/Nav";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12">
+      <div className="container mx-auto">
+        <Link href="/" className="text-4xl font-semibold">
+          Advaith<span className="text-accent-default">.</span>
+        </Link>
+        {/* Desktop Nav */}
+        <div className="hidden xl:flex">
+          <Nav />
+          <Link href="/contact">
+            <Button>Hire!</Button>
+          </Link>
+        </div>
+        {/* Mobile Nav */}
+        <div className="xl:hidden">mobile nav</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
