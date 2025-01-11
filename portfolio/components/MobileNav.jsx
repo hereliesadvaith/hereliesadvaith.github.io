0a48d8d36614c563c27e90156e3f11d63f61633f
Advaith B G <hereliesadvaith@gmail.com>
2025-01-12 03:55:11 +0530

    [UPD] Mobile nav items.

--- portfolio/components/MobileNav.jsx
+++ portfolio/components/MobileNav.jsx
@@ -6,12 +6,13 @@ import {
   SheetDescription,
   SheetTitle,
   SheetTrigger,
+  SheetClose,
 } from "@/components/ui/sheet";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { CiMenuFries } from "react-icons/ci";
 
-const Links = [
+const links = [
   {
     name: "Home",
     path: "/",
@@ -48,7 +49,35 @@ const MobileNav = () => {
       <SheetContent>
         <SheetTitle className="hidden" />
         <SheetDescription className="hidden" />
-        <div>Logo</div>
+        <SheetContent className="flex">
+          {/* logo */}
+          <div className="mt-32 mb-24 text-center text-2xl">
+            <Link href="/">
+              <h1 className="text-4xl font-semibold">
+                Advaith<span className="text-accent-default">.</span>
+              </h1>
+            </Link>
+          </div>
+          {/* nav */}
+          <nav className="flex flex-col ml-20 items-baseline gap-6">
+            {links.map((link, index) => {
+              return (
+                <SheetClose asChild key={index}>
+                  <Link
+                    href={link.path}
+                    className={`${
+                      link.path === pathname
+                        ? "text-accent-default border-b-2 border-accent-default inline"
+                        : ""
+                    } text-xl capitalize hover:text-accent-default transition-all`}
+                  >
+                    {link.name}
+                  </Link>
+                </SheetClose>
+              );
+            })}
+          </nav>
+        </SheetContent>
       </SheetContent>
     </Sheet>
   );
