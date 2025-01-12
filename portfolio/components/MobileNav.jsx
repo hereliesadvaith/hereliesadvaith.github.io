ef17eee2af2cef228a8b54327d2730d697d89b41
Advaith B G <hereliesadvaith@gmail.com>
2025-01-13 04:21:42 +0530

    [UPD] Mobile nav items.

--- portfolio/components/MobileNav.jsx
+++ portfolio/components/MobileNav.jsx
@@ -46,38 +46,36 @@ const MobileNav = () => {
       <SheetTrigger>
         <CiMenuFries className="text-3xl text-accent-default" />
       </SheetTrigger>
-      <SheetContent>
+      <SheetContent className="flex">
         <SheetTitle className="hidden" />
         <SheetDescription className="hidden" />
-        <SheetContent className="flex">
-          {/* logo */}
-          <div className="mt-32 mb-24 text-center text-2xl">
-            <Link href="/">
-              <h1 className="text-4xl font-semibold">
-                Advaith<span className="text-accent-default">.</span>
-              </h1>
-            </Link>
-          </div>
-          {/* nav */}
-          <nav className="flex flex-col ml-20 items-baseline gap-6">
-            {links.map((link, index) => {
-              return (
-                <SheetClose asChild key={index}>
-                  <Link
-                    href={link.path}
-                    className={`${
-                      link.path === pathname
-                        ? "text-accent-default border-b-2 border-accent-default inline"
-                        : ""
-                    } text-xl capitalize hover:text-accent-default transition-all`}
-                  >
-                    {link.name}
-                  </Link>
-                </SheetClose>
-              );
-            })}
-          </nav>
-        </SheetContent>
+        {/* logo */}
+        <div className="mt-32 mb-24 text-center text-2xl">
+          <Link href="/">
+            <h1 className="text-4xl font-semibold">
+              Advaith<span className="text-accent-default">.</span>
+            </h1>
+          </Link>
+        </div>
+        {/* nav */}
+        <nav className="flex flex-col ml-20 items-baseline gap-6">
+          {links.map((link, index) => {
+            return (
+              <SheetClose asChild key={index}>
+                <Link
+                  href={link.path}
+                  className={`${
+                    link.path === pathname
+                      ? "text-accent-default border-b-2 border-accent-default inline"
+                      : ""
+                  } text-xl capitalize hover:text-accent-default transition-all`}
+                >
+                  {link.name}
+                </Link>
+              </SheetClose>
+            );
+          })}
+        </nav>
       </SheetContent>
     </Sheet>
   );
