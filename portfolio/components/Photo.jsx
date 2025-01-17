666bbdb069193ea1cefa6924822d0161a7c63a31
Advaith B G <hereliesadvaith@gmail.com>
2025-01-17 05:57:29 +0530

    [UPD] Framer motion ring.

--- portfolio/components/Photo.jsx
+++ portfolio/components/Photo.jsx
@@ -9,10 +9,18 @@ const Photo = () => {
         initial={{ opacity: 0 }}
         animate={{
           opacity: 1,
-          transition: { duration: 0.4, delay: 0, ease: "easeIn" },
+          transition: { delay: 1, duration: 0.4, ease: "easeIn" },
         }}
       >
-        <div className="overflow-hidden relative rounded-full w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten">
+        {/* Image */}
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 1.4, duration: 0.4, ease: "easeInOut" },
+          }}
+          className="overflow-hidden rounded-full w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
+        >
           <Image
             src="/assets/home/dp.png"
             priority
@@ -21,7 +29,34 @@ const Photo = () => {
             alt=""
             className="object-contain"
           />
-        </div>
+        </motion.div>
+        {/* Circle */}
+        <motion.svg
+          className="w-[300px] xl:w-[506px] h-300px xl:h-[506px] "
+          fill="transparent"
+          viewBox="0 0 506 506"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.circle
+            cx="253"
+            cy="253"
+            r="250"
+            stroke="#F2F2F2"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            initial={{ strokeDasharray: "24 10 0 0" }}
+            animate={{
+              strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
+              rotate: [120, 360],
+            }}
+            transition={{
+              duration: 10,
+              repeat: Infinity,
+              repeatType: "reverse",
+            }}
+          />
+        </motion.svg>
       </motion.div>
     </div>
   );
