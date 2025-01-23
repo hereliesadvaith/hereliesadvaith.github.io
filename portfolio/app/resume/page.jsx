c47ab0064c7759013085326f4a014b56e5b49ec2
Advaith B G <hereliesadvaith@gmail.com>
2025-01-23 17:47:42 +0530

    [ADD] constants.

--- portfolio/app/resume/page.jsx
+++ portfolio/app/resume/page.jsx
@@ -1,5 +1,39 @@
+"use client";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { delay, motion } from "framer-motion";
+
+// Data
+import { about, experience, education, skills } from "@/constants/resume";
+
 const Resume = () => {
-  return <div>Resume Page</div>;
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transform: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+    >
+      <div className="container mx-auto">
+        <Tabs defaultValue="about" className="flex flex-col xl:flex-row gap-[60px]">
+          <TabsList>
+            <TabsTrigger>About Me</TabsTrigger>
+            <TabsTrigger>Experience</TabsTrigger>
+            <TabsTrigger>Education</TabsTrigger>
+            <TabsTrigger>Skills</TabsTrigger>
+          </TabsList>
+        </Tabs>
+      </div>
+    </motion.div>
+  );
 };
 
 export default Resume;
